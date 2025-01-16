f0ac17ffd59e521a4b2d9c6afb417f84026ce983
Jeff Ripke <jripke74@gmail.com>
2025-01-15 16:54:03 -0800

    add 0 check

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -35,7 +35,7 @@ function calculateResult(calculationType) {
       calculationType !== "SUBTRACT" &&
       calculationType !== "MULTIPLY" &&
       calculationType !== "DIVIDE") ||
-    enteredNumber === 0
+      !enteredNumber
   ) {
     return;
   }
